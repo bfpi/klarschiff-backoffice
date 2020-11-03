03645a2b131ba178fc1f0655fe1a5737e0c77f03
Ricardo Oest <oest@bfpi.de>
2020-11-03 14:06:42 +0100

    Redmine #2704: WIP Jobübersicht

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class JobsController < ApplicationController
-  def index; end
+  def index
+    @date = params[:date].present? ? Date.new(params[:date]) : Date.current + 2.days
+    @grouped_jobs = Job.group_by_group(@date)
+  end
 end
