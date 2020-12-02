0dea6af348e95ddf79c272224bdf888276e3d3f8
Niels Bennke <bennke@bfpi.de>
2020-12-02 08:54:17 +0100

    Fix fixtures and test

--- test/jobs/calculate_average_turnaround_time_job_test.rb
+++ test/jobs/calculate_average_turnaround_time_job_test.rb
@@ -4,6 +4,6 @@ require 'test_helper'
 
 class CalculateAverageTurnaroundTimeJobTest < ActiveJob::TestCase
   test 'that job is performable' do
-    assert_nothing_raised { FCalculateAverageTurnaroundTimeJo.perform_now }
+    assert_nothing_raised { CalculateAverageTurnaroundTimeJob.perform_now }
   end
 end
