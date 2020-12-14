093b5e3dfaaccbd93717f6401e8c7cee326a096b
ro90 <oest@bfpi.de>
2020-12-14 08:34:07 +0100

    Redmine #2708: Direktlink Meldung via Nummer und „Schnellsuchfeld“
    Realisierung der Funktion zum direkten Öffnen einer Meldung mit vorhandener Nummer

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -58,3 +58,12 @@ $ ->
     ids = $($(@).data('table')).find('.selectable').toArray().filter((e) -> e.checked).map((e) -> e.value)
     return if ids.length == 0
     updateMultipleJobs $.param({ job_ids: ids, job: { date: $($(@).data('field')).val() } })
+  
+  $(document).on 'keypress', '#search_issue', (e) ->
+    if (e.key == 'Enter' || e.keyCode == 13)
+      $.get
+        url: "/issues/#{@.value}/edit"
+        method: 'GET'
+        dataType: 'script'
+    else
+      e.preventDefault() unless e.key.match(/[0-9]/)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,24 @@
 
 class ApplicationController < ActionController::Base
   rescue_from StandardError, with: :respond_with_error
-
+  rescue_from ActiveRecord::RecordNotFound, with: :respond_with_not_found
   include Authorization
 
   private
 
   def respond_with_error(error)
     raise if Rails.env.test?
-
     logger.error error.inspect + error.backtrace.join("\n ")
-
-    @error = error
+    @message = error.message
     render :exception
+  end
+
+  def respond_with_not_found
+    raise if Rails.env.test?
+    @message = I18n.t(:record_not_found)
+    respond_to do |format|
+      format.js { render :exception, formats: :js }
+      format.html { render :exception }
+    end
   end
 end
