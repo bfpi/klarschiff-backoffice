c10a7846791a790716c6e0e0a3cbd25f4f898c34
Niels Bennke <bennke@bfpi.de>
2021-04-01 13:23:30 +0200

    Fix: https://github.com/bfpi/klarschiff-backoffice/pull/5#discussion_r543669176

--- app/jobs/calculate_average_turnaround_time_job.rb
+++ app/jobs/calculate_average_turnaround_time_job.rb
@@ -4,23 +4,32 @@ class CalculateAverageTurnaroundTimeJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    Category.all.each do |category|
-      next if (times = category_times(category)).blank?
-      category.average_turnaround_time = Time.at(times.sum / times.count).utc.day - 1
-      category.save
+    Category.find_each do |category|
+      next if (days = average_turnaround_days(category.id)).blank?
+      category.update! average_turnaround_time: days
     end
   end
 
   private
 
-  def category_times(category)
-    LogEntry.where(table: :issue, attr: :status, old_value: :in_process,
-                   issue: category.issues).map { |log_entry| calculate_turnaround_time(log_entry) }.flatten
-  end
-
-  def calculate_turnaround_time(log_entry)
-    log_entry.issue.all_log_entries.where(table: :issue, attr: :status, new_value: :in_process)
-      .where('created_at < ?', log_entry.created_at)
-      .order(created_at: :desc).limit(1).map { |tmp| (log_entry.created_at - tmp.created_at) }
+  def average_turnaround_days(category_id)
+    LogEntry.connection.select_value <<~SQL.squish
+      SELECT EXTRACT(DAY FROM AVG("diff"))::INTEGER
+      FROM (
+        SELECT DISTINCT ON ("le"."id") "le"."created_at" - "prev"."created_at" "diff"
+        FROM #{LogEntry.quoted_table_name} "le"
+          JOIN #{LogEntry.quoted_table_name} "prev" ON
+            "prev"."issue_id" = "le"."issue_id" AND
+            "prev"."created_at" < "le"."created_at" AND
+            "prev"."table" = 'issue' AND
+            "prev"."attr" = 'status' AND
+            "prev"."new_value" = 'in_process'
+        WHERE "le"."table" = 'issue' AND
+          "le"."attr" = 'status' AND
+          "le"."old_value" = 'in_process' AND
+          "le"."issue_id" IN (SELECT "id" FROM #{Issue.quoted_table_name} WHERE "category_id" = #{category_id})
+        ORDER BY "le".id, "prev"."created_at" DESC
+      ) s
+    SQL
   end
 end
