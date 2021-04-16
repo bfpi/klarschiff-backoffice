90e4f852234f6ca2e847e8bb8dce7d061704881e
rvoss <voss@bfpi.de>
2021-04-16 14:35:41 +0200

    CitySDK

---
+++ app/controllers/concerns/citysdk_parameter_validation.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module CitysdkParameterValidation
+  extend ActiveSupport::Concern
+
+  included do
+    private_instance_methods.select { |m| m.to_s.start_with?('validate_') }.each do |method|
+      before_action method
+    end
+  end
+
+  private
+
+  def validate_dates
+    %w[start_date end_date updated_after updated_before].each do |date|
+      next if params[date].blank?
+      y, m, d = params[date].split '-'
+      next if Date.valid_date? y.to_i, m.to_i, d.to_i
+      raise "date #{date} invalid"
+    end
+  end
+
+  def validate_status
+    return unless params[:status].present? && !Citysdk::Status.valid_filter_values(params[:status])
+    raise 'status invalid'
+  end
+
+  def validate_detailed_status
+    return unless params[:detailed_status].present? && !Citysdk::Status.valid_filter_values(params[:detailed_status],
+      :citysdk)
+    raise 'status invalid'
+  end
+
+  def validate_service_request_id
+    return unless params[:service_request_id].present? && !params[:service_request_id].i?
+    raise 'service_request_id is not a number'
+  end
+end
