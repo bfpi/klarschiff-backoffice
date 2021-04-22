9deb8a4e69001de825c77a38403f255d4749531c
Niels Bennke <bennke@bfpi.de>
2021-04-22 12:11:00 +0200

    Vor der Validierung alle Textfelder von führenden und nachgestellten Leerzeichen befreien

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  before_validation :strip_input_fields
+
   def self.human_enum_name(enum_name, enum_value)
     I18n.t("enums.#{model_name.i18n_key}.#{enum_name}.#{enum_value}")
+  end
+
+  def strip_input_fields
+    attributes.each do |name, value|
+      self[name] = value.strip if value.respond_to?(:strip) && column_for_attribute(name).type == :text
+    end
   end
 end
