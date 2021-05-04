0f64f6a94015adbcc197519c370c6745d4e58cf3
Niels Bennke <bennke@bfpi.de>
2021-05-04 21:25:15 +0200

    Import Bilder zu Meldungen (Überarbeitung 99421a0)
    Aufruf:
    ```
    rails r "ImportIssueImagesJob.perform_now('images_csv', '/path/to/images')"
    ```
    
    Hinweise:
    * Der Pfad zu den Bildern sollte entweder ein relativer Pfad ausgehend vom `Rails.root` oder ein absoluter Pfad sein. Eine `~` zur Auflösung des HOME_DIRS des aktuellen Nutzers wird nicht unterstützt.
    * Die Spaltenbezeichnung der CSV ist grundsätzlich egal, der Importer geht jedoch von folgenen Inhalten in folgender Reihenfolge aus:
     * issue_issue_id,
     * issue_photo_name,
     * issue_photo_status,
     * photo_issue_id,
     * photo_photo_name,
     * photo_author,
     * photo_confirmation_hash,
     * photo_confirmed_at,
     * photo_created_at

---
+++ app/jobs/import_issue_images_job.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+class ImportIssueImagesJob < ApplicationJob
+  queue_as :default
+
+  def perform(images_csv, images_path)
+    CSV.table(images_csv).each do |row|
+      if (issue = Issue.find_by(id: row[0])).blank?
+        print_error row[0], row[1]
+        next
+      end
+      import_image issue, row, images_path
+    end
+  end
+
+  private
+
+  def import_image(issue, row, images_path)
+    photo = issue.photos.new(status: row[2], author: row[5], confirmation_hash: row[6], confirmed_at: row[7],
+                             created_at: row[8])
+    photo.file.attach io: File.open(Pathname.new(images_path).join(row[1])), filename: row[1], content_type: 'image/jpg'
+    photo.save!
+  end
+
+  def print_error(issue_id, file_name)
+    puts "Meldung ##{issue_id} konnte zu '#{file_name}' nicht gefunden werden." # rubocop: disable Rails/Output
+  end
+end
