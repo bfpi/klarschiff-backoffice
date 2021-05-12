3571fe9ad905c4f006b55d3f96bf91d31e34fab6
Niels Bennke <bennke@bfpi.de>
2021-05-12 08:43:35 +0000

    Umgebung für `consolidation` ergänzt und Überlagerung ermöglicht

--- config/environments/consolidation.rb
+++ config/environments/consolidation.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "klarschiff_backoffice_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
