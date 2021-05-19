2b391f61ed6f01315c573acceb47362947905053
Ricardo Oest <oest@bfpi.de>
2021-05-19 07:30:41 +0200

    Redmine #2713: Umsetzung Rückmeldungen

--- app/jobs/inform_editorial_staff_on_issues_job.rb
+++ app/jobs/inform_editorial_staff_on_issues_job.rb
@@ -20,7 +20,7 @@ class InformEditorialStaffOnIssuesJob < ApplicationJob
       next if (issues = send(method, time - Jobs::Issue.send("#{method}_days").days)).blank?
       @issues[method] = issues.to_a
     end
-    %i[not_solvable_no_status_note not_open_not_accepted description_or_photo_not_released
+    %i[not_solvable_no_status_note not_open_not_accepted description_and_photo_not_released
        no_responsibility].each do |method|
       next if (issues = send(method)).blank?
       @issues[method] = issues.to_a
@@ -28,34 +28,55 @@ class InformEditorialStaffOnIssuesJob < ApplicationJob
   end
 
   def open_not_accepted(time)
-    Issue.status_received.where(responsibility_accepted: false).where(id: latest_attr_change(time, 'group'))
+    Issue.not_archived.status_received.where(responsibility_accepted: false)
+      .where(id: latest_attr_change(time, 'group'))
   end
 
   def in_process_no_status_note(time)
-    Issue.status_in_process.where(status_note: nil).where(id: latest_attr_change(time, 'status'))
+    Issue.not_archived.status_in_process.where(status_note: nil).where(id: latest_attr_change(time, 'status'))
   end
 
   def ideas_without_min_supporters(time)
-    Issue.status_open.ideas_without_min_supporters.where(id: latest_attr_change(time, 'group'))
+    open_issues.ideas_without_min_supporters.where(id: latest_attr_change(time, 'group'))
+  end
+
+  def open_issues
+    Issue.includes(ideas_includes).references(ideas_includes).left_joins(:supporters)
+      .group(ideas_group_by).not_archived.status_open
+  end
+
+  def ideas_includes
+    [:abuse_reports, :group, :delegation, :job, :photos, { category: %i[main_category sub_category] }]
+  end
+
+  def ideas_group_by
+    [
+      Category.arel_table[:id], 'delegation_issue.id', Group.arel_table[:id],
+      Issue.arel_table[:id], Job.arel_table[:id], MainCategory.arel_table[:id],
+      Photo.arel_table[:id], SubCategory.arel_table[:id], AbuseReport.arel_table[:id]
+    ]
   end
 
   def in_process(time)
-    Issue.status_in_process.where(iat[:created_at].lt(time))
+    Issue.not_archived.status_in_process.where(iat[:created_at].lt(time))
   end
 
   def not_solvable_no_status_note
-    Issue.status_not_solvable.where(status_note: nil)
+    Issue.not_archived.status_not_solvable.where(status_note: nil)
   end
 
   def not_open_not_accepted
-    Issue.where(status: %w[reviewed in_process], responsibility_accepted: false)
+    Issue.not_archived.where(status: %w[reviewed in_process], responsibility_accepted: false)
   end
 
-  def description_or_photo_not_released
-    Issue.status_received.where(description_status: 'internal').reject { |is| is.photos.status_external.exists? }
+  def description_and_photo_not_released
+    Issue.not_archived.status_received.where(description_status: 'internal')
+      .reject { |is| is.photos.status_external.exists? }
   end
 
   def no_responsibility
-    Issue.where(iat[:archived_at].eq(nil).and(iat[:status].not_eq('pending')).and(iat[:group_id].eq(nil)))
+    Issue.not_archived.where(
+      iat[:archived_at].eq(nil).and(iat[:status].not_eq('pending')).and(iat[:group_id].eq(nil))
+    )
   end
 end
