c6a6e9d4a7e21084a5a8aa3de8b84281bc55dcc4
nbennke <bennke@bfpi.de>
2021-05-28 12:40:57 +0200

    Apply suggestions from code review

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module KlarschiffBackoffice
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
     config.active_record.pluralize_table_names = false
 
     config.assets.precompile += %w[ol.css]
