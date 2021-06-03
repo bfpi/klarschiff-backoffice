22f7afd6a36d1509ec0165e76cb6a8a929fd300f
rvoss <voss@bfpi.de>
2021-06-03 14:58:37 +0200

    Fix: Citysdk District

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class District < ApplicationRecord
-  include Citysdk::Serialization
-
   belongs_to :authority
 
   validates :area, :name, :regional_key, presence: true
+  validates :name, uniqueness: { scope: :authority }
   validates :name, uniqueness: { conditions: -> { where authority: authority } }
 
   self.serialization_attributes = %i[id name grenze]
