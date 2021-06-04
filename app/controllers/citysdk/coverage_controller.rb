1ee0d066835d9aea65700bb9cd0625f82c23d390
rvoss <voss@bfpi.de>
2021-06-04 11:04:55 +0200

    Meldung innerhalb/außerhalb Gültigkeitsgebiet

--- app/controllers/citysdk/coverage_controller.rb
+++ app/controllers/citysdk/coverage_controller.rb
@@ -7,15 +7,17 @@ module Citysdk
     end
 
     def response_data
-      instances = Instance.where('ST_Within(ST_SetSRID(ST_MakePoint(:long, :lat), 4326), area)',
-        { lat: params[:lat].to_f, long: params[:long].to_f }).order(:instance_url)
-
-      if instances.blank? || (instance_url = instances.filter_map(&:instance_url)).present?
+      if instances.blank? || (instance_url = instances.filter_map { |fm| fm if fm.instance_url.present? }).present?
         rd = { result: false }
         rd[:instance_url] = instance_url.blank? ? Settings::Instance.parent_instance_url : instance_url.first
         return rd
       end
       { result: true }
+    end
+
+    def instances
+      @instances ||= Instance.where('ST_Within(ST_SetSRID(ST_MakePoint(:long, :lat), 4326), area)',
+        { lat: params[:lat].to_f, long: params[:long].to_f }).order(:instance_url)
     end
   end
 end
