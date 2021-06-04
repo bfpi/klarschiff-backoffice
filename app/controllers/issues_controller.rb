c0edae06c3379d4fda8f2f453aee8a5ec5c72030
Ricardo Oest <oest@bfpi.de>
2021-06-04 09:32:46 +0200

    Redmine #2702 + #2703: Kartenansicht löschen, wenn Modal geöffnet wird. Refactoring

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,17 +2,9 @@
 
 class IssuesController < ApplicationController
   include Export
+  include Index
 
   before_action :set_tab
-
-  def index
-    respond_to do |format|
-      format.json { render json: results.to_json }
-      format.js { @issues = paginate(results) }
-      format.html { html_response }
-      format.xlsx { xlsx_response }
-    end
-  end
 
   def show
     @edit_issue_url = edit_issue_url(params[:id])
@@ -62,41 +54,12 @@ class IssuesController < ApplicationController
     tabs + %i[comment abuse_report map photo log_entry]
   end
 
-  def base_collection
-    Issue.includes(:abuse_reports, :group, :delegation, category: %i[main_category sub_category])
-      .order created_at: :desc
-  end
-
-  def html_response
-    return @issues = paginate(results) unless params[:show_map] == 'true'
-    @filter = params.permit(*permitted_filter_attributes)
-    logger.info "TESTING #{@filter.inspect}"
-    @extended_filter = params[:extended_filter] == 'true'
-    render :map
-  end
-
-  def xlsx_response
-    @issues = paginate(results)
-    xlsx_export
-  end
-
-  def results
-    @extended_filter = params[:extended_filter] == 'true'
-    @filter = params.permit(*permitted_filter_attributes)
-    @status = (params[:status] || 0).to_i
-    IssueFilter.new(params).collection
-  end
-
   def feedbacks(issue)
     issue.feedbacks.order(created_at: :desc).page(params[:page] || 1).per params[:per_page] || 10
   end
 
   def log_entries(issue)
     issue.all_log_entries.order(created_at: :desc).page(params[:page] || 1).per params[:per_page] || 10
-  end
-
-  def paginate(issues)
-    issues.page(params[:page] || 1).per(params[:per_page] || 20)
   end
 
   def issue_params
