e74de69fba50614f2436f614a1310d405dc53259
rvoss <voss@bfpi.de>
2021-06-08 09:11:08 +0200

    Redmin #2735: Missbrauchsmeldung

--- app/models/abuse_report.rb
+++ app/models/abuse_report.rb
@@ -10,7 +10,7 @@ class AbuseReport < ApplicationRecord
 
   validates :message, presence: true
 
-  default_scope -> { order created_at: :desc }
+  default_scope -> { where.not(confirmed_at: nil).order(created_at: :desc) }
 
   def to_s
     "#{I18n.l(created_at, format: :no_seconds)}#{I18n.t('statuses.abuse_report.open') unless resolved_at?}"
