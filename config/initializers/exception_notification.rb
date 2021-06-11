5f1b2962823ca9a7940683c11f38b2fdbe889c58
rvoss <voss@bfpi.de>
2021-06-11 06:46:33 +0000

    Exception-Mails

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -4,8 +4,8 @@ unless Rails.env.development? || Rails.env.test?
   config = Config.for(:mailer)
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
-      email_prefix: '[KS-Backoffice] ',
-      sender_address: ["\"Klarschiff-Backoffice (#{ENV['ENVIRONMENT']})\" <#{config[:default][:from]}>"],
+      email_prefix: '[KS-Backoffice Exception] ',
+      sender_address: ["\"Klarschiff-Backoffice (#{Rails.env.to_s})\" <#{config[:default][:from]}>"],
       exception_recipients: config[:default][:exceptions_recipient],
       email_headers: { 'X-Override-Consolidation': true }
     }
