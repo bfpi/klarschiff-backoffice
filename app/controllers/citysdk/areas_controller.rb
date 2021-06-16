88f4d0ece5d04da579aaf5f814583e0d3ff53455
Niels Bennke <bennke@bfpi.de>
2021-06-16 14:44:12 +0200

    Fix `Areas` und `Requests` Anfragen für Statistik

--- app/controllers/citysdk/areas_controller.rb
+++ app/controllers/citysdk/areas_controller.rb
@@ -19,7 +19,7 @@ module Citysdk
       search_class = Settings.main_instance? ? Citysdk::Authority : Citysdk::District
       response = [Instance.first]
       response = search_class.all if params[:with_districts].present? && params[:area_code].blank?
-      response = search_class.find(params[:area_code].split(',')) if params[:area_code].present?
+      response = search_class.where(id: params[:area_code].split(',')) if params[:area_code].present?
       response
     end
 
