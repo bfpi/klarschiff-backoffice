a92600bd97a1f1f4ea8bd7ddcae23bffede0a3c2
Niels Bennke <bennke@bfpi.de>
2021-06-18 13:00:22 +0200

    404 Rückmeldungen ohne den `internal_server_error` Präfix ausgeben

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,18 +13,20 @@ class ApplicationController < ActionController::Base
   def respond_with_error(error)
     raise if Rails.env.test?
     logger.error "#{error.inspect}\n#{error.backtrace.join "\n "}"
-    @message = error.message
     ExceptionNotifier.notify_exception(error, env: request.env)
-    render :exception
+    respond_with_execption error.message
   end
 
   def respond_with_not_found
     raise if Rails.env.test?
-    respond_with_execption I18n.t('activerecord.errors.record_not_found')
+    respond_with_execption I18n.t('activerecord.errors.record_not_found'), skip_internal_error_prefix: true
   end
 
-  def respond_with_execption(message)
-    @message = message
+  def respond_with_execption(message, skip_internal_error_prefix: false)
+    msg = []
+    msg << I18n.t(:internal_server_error) unless skip_internal_error_prefix
+    msg << message.truncate(200)
+    @message = msg.join(' ')
     respond_to do |format|
       format.js { render :exception, formats: :js }
       format.html { render :exception }
--- app/views/application/exception.js.coffee
+++ app/views/application/exception.js.coffee
@@ -1,6 +1,6 @@
-message = '<%= j("#{t(:internal_server_error)} #{@message}") %>'
+message = '<%= j @message %>'
 if ($('#modal').hasClass('hide'))
-  $('.content.container').prepend('<div class="alert alert-danger" role="alert">' + message + '</div>')
+  $('.content.container').prepend("<div class='alert alert-danger' role='alert'>#{message}</div>")
 else
   $('#modal .modal-body').html(message)
   $('#modal').modal()
