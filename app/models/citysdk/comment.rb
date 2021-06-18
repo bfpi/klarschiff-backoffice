2451c954a9706715d7bbae2968934cee33b6208b
Niels Bennke <bennke@bfpi.de>
2021-06-18 08:51:19 +0200

    Fix ActionView::Template::Error (undefined method `author` for #<Comment:>) im PPC Frontend Client

--- app/models/citysdk/comment.rb
+++ app/models/citysdk/comment.rb
@@ -4,7 +4,7 @@ module Citysdk
   class Comment < ::Feedback
     include Citysdk::Serialization
 
-    self.serialization_attributes = %i[id jurisdiction_id comment datetime service_request_id]
+    self.serialization_attributes = %i[id jurisdiction_id author comment datetime service_request_id]
     alias_attribute :service_request_id, :issue_id
     alias_attribute :comment, :message
     alias_attribute :datetime, :created_at
