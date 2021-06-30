5f771f2f14719e784cdef39314a393ef980043fc
rvoss <voss@bfpi.de>
2021-06-30 11:19:31 +0200

    Fix: Redmine #2728 - Link berichtigt

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -3,7 +3,7 @@
 class ConfirmationMailer < ApplicationMailer
   def issue(to:, issue_id:, confirmation_hash:)
     @confirmation_url = "#{mailer_config[:confirmation_base_url]}/#{confirmation_hash}/issue"
-    @delete_url = "#{mailer_config[:confirmation_base_url]}/#{confirmation_hash}/delete_issue"
+    @delete_url = "#{mailer_config[:confirmation_base_url]}/#{confirmation_hash}/revoke_issue"
     @issue_url = Settings::Instance.frontend_issue_url % issue_id
     mail(to: to, interpolation: { subject: { number: issue_id } })
   end
