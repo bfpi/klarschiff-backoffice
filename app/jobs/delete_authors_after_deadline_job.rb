07b479f8cf1ca54fb73bb14d403c3f7483d08d4a
rvoss <voss@bfpi.de>
2021-07-12 08:37:52 +0200

    Redmine #3156: Entfernte Mailadressen erklären

--- app/jobs/delete_authors_after_deadline_job.rb
+++ app/jobs/delete_authors_after_deadline_job.rb
@@ -13,15 +13,15 @@ class DeleteAuthorsAfterDeadlineJob < ApplicationJob
 
   # rubocop:disable Rails/SkipsModelValidations
   def remove_author(issue)
-    issue.update(author: nil)
-    issue.abuse_reports.update_all(author: nil)
-    issue.photos.update_all(author: nil)
-    issue.feedbacks.update_all(author: nil)
+    issue.update(author: JobSettings::Issue.author_deletion_replacement)
+    issue.abuse_reports.update_all(author: JobSettings::Issue.author_deletion_replacement)
+    issue.photos.update_all(author: JobSettings::Issue.author_deletion_replacement)
+    issue.feedbacks.update_all(author: JobSettings::Issue.author_deletion_replacement)
   end
   # rubocop:enable Rails/SkipsModelValidations
 
   def deletion_conds(time)
-    iat[:archived_at].not_eq(nil).and(iat[:archived_at].lt(time))
+    iat[:archived_at].not_eq(JobSettings::Issue.author_deletion_replacement).and(iat[:archived_at].lt(time))
   end
 
   def iat
