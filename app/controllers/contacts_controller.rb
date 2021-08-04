25d38ff0df370668dbb244ffd34462673a83d0e6
Niels Bennke <bennke@bfpi.de>
2021-08-04 21:28:57 +0200

    Kontakt und Impressum auch ohne Login anzeigen

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
+  skip_before_action :authenticate
+
   def show; end
 end
--- app/controllers/imprints_controller.rb
+++ app/controllers/imprints_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ImprintsController < ApplicationController
+  skip_before_action :authenticate
+
   def show; end
 end
