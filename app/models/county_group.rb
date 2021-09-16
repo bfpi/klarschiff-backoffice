2b74f3f8234957a609b8bd3761ec1689485b8fc0
Niels Bennke <bennke@bfpi.de>
2021-09-16 13:30:24 +0200

    `reference_name` für `CountyGroup` ohne Präfix

--- app/models/county_group.rb
+++ app/models/county_group.rb
@@ -2,4 +2,10 @@
 
 class CountyGroup < Group
   belongs_to :county, foreign_key: :reference_id, inverse_of: :groups
+
+  private
+
+  def reference_name
+    county.to_s with_model_name: false
+  end
 end
