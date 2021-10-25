7ec8c07d3967698a361a026bffd148a234b7f837
rvoss <voss@bfpi.de>
2021-10-25 10:20:05 +0200

    Redmine #3254: Sortierung Kategorien nicht vollständig alphabetisch

--- app/controllers/citysdk/services_controller.rb
+++ app/controllers/citysdk/services_controller.rb
@@ -3,7 +3,7 @@
 module Citysdk
   class ServicesController < CitysdkController
     def index
-      citysdk_response(Service.all,
+      citysdk_response(sorted,
         { root: :services, element_name: :service, document_url: authorized?(:citysdk_d3_document_url) })
     end
 
@@ -11,6 +11,13 @@ module Citysdk
       service_definition = Citysdk::ServiceDefinition.where(id: params[:id])
       citysdk_response(service_definition, root: :service_definition, element_name: :service,
                                            document_url: authorized?(:citysdk_d3_document_url))
+    end
+
+    private
+
+    def sorted
+      Service.all.joins(:main_category, :sub_category).includes(:main_category, :sub_category)
+        .order('main_category.name': :asc, 'sub_category.name': :asc)
     end
   end
 end
