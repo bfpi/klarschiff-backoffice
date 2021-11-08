b88387d13552357661409d7d78999e7680387d76
Niels Bennke <bennke@bfpi.de>
2021-11-08 10:37:41 +0100

    Moved view helper methods to `ApplicationHelper`

--- app/controllers/concerns/sorting.rb
+++ app/controllers/concerns/sorting.rb
@@ -4,9 +4,10 @@ module Sorting
   extend ActiveSupport::Concern
 
   included do
-    helper_method :order_link
-    delegate :tag, to: 'ActionController::Base.helpers'
+    helper_method :order_params
   end
+
+  private
 
   def order_params
     params.fetch(:order_by, {}).permit(%i[column dir])
@@ -23,20 +24,5 @@ module Sorting
 
   def order_dir
     order_params[:dir]&.match?(/^(a|de)sc$/i) ? order_params[:dir] : :asc
-  end
-
-  def order_link(label, controller, col)
-    url = url_for(controller: controller, order_by: { column: col, dir: order_link_dir(col) })
-    tag.a((label + order_dir_icon(col)).html_safe, href: url) # rubocop:disable Rails/OutputSafety
-  end
-
-  def order_dir_icon(col)
-    return '' if order_params.blank? || order_params[:column].to_sym != col.to_sym
-    tag.span('', class: "mx-2 fa fa-chevron-#{order_params[:dir].to_sym == :asc ? 'up' : 'down'}")
-  end
-
-  def order_link_dir(col)
-    return :asc if order_params.blank? || order_params[:column].to_sym != col.to_sym
-    order_params[:dir].to_sym == :asc ? :desc : :asc
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
   def icon(color, kind: 'blank')
     "icons/list/png/#{kind}-#{color}-22px.png"
   end
+
+  def order_link(label, controller, col)
+    url = url_for(controller: controller, order_by: { column: col, dir: order_link_dir(col) })
+    tag.a((label + order_dir_icon(col)).html_safe, href: url) # rubocop:disable Rails/OutputSafety
+  end
+
+  def order_dir_icon(col)
+    return '' if order_params.blank? || order_params[:column].to_sym != col.to_sym
+    tag.span('', class: "mx-2 fa fa-chevron-#{order_params[:dir].to_sym == :asc ? 'up' : 'down'}")
+  end
+
+  def order_link_dir(col)
+    return :asc if order_params.blank? || order_params[:column].to_sym != col.to_sym
+    order_params[:dir].to_sym == :asc ? :desc : :asc
+  end
 end
