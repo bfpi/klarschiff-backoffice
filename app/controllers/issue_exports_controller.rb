2ad61122679bbcc490dbb77086edf7558271f298
ro3t <voss@bfpi.de>
2021-11-11 15:03:55 +0100

    Redmine #3272: Drucken einer Meldung mündet in Fehlermeldung (#94)
    Überarbeitung inkl. Layout und Erzeugung

--- app/controllers/issue_exports_controller.rb
+++ app/controllers/issue_exports_controller.rb
@@ -6,8 +6,9 @@ class IssueExportsController < ApplicationController
   def create
     @issue = Issue.find(params[:issue_id])
     @map_image = Base64.decode64(params[:map_image])
-    kit = PDFKit.new(render_to_string(template: 'issue_exports/create', formats: :html, layout: false).to_s,
-      page_size: 'A4', margin_top: '0.5in', margin_right: '0.5in', margin_bottom: '0.5in', margin_left: '0.5in')
+    kit = PDFKit.new(render_to_string(template: 'issue_exports/create', formats: :html, layout: false),
+      root_url: "#{request.base_url}/", page_size: 'A4', margin_top: '0.5in', margin_right: '0.5in',
+      margin_bottom: '0.5in', margin_left: '0.5in')
     headers['X-FileName'] = (file_name = "Meldung_#{@issue.id}.pdf")
     send_data kit.to_pdf, filename: file_name, disposition: :attachment
   end
