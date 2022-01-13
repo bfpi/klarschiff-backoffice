ea713235eb10874da4985a86b526ff6fcf1cc1d1
rvoss <voss@bfpi.de>
2022-01-13 10:31:14 +0100

    Redmine #3288: mailto-Weiterleitung via externem E-Mail-Client

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class IssueMailer < ApplicationMailer
-  helper :application, :issues, :issue_emails
+  helper :application, :issues
 
   def issue(issue_email:)
     @issue_email = issue_email
