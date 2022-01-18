1d64f4a7ad61b2abf97db4e94adc957bb92bff1d
Ricardo Oest <oest@bfpi.de>
2022-01-18 11:15:47 +0100

    #3345: Passwort-Validierung in als Validator-Klasse ausgelagert

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -22,40 +22,4 @@ class ApplicationRecord < ActiveRecord::Base
       self[name] = value.strip if value.respond_to?(:strip) && column_for_attribute(name).type == :text
     end
   end
-
-  class PasswordValidator < ActiveModel::EachValidator
-    NUMBER_FORMAT = /\d/
-    LOWERCASE_FORMAT = /[a-z]/
-    CAPITAL_FORMAT = /[A-Z]/
-
-    def validate_each(record, attribute, value)
-      record.errors.add(attribute, :invalid) unless password_valid?(value)
-    end
-
-    private
-
-    def password_valid?(password)
-      password.length >= 8 && included_characters(password) == 3
-    end
-
-    def included_characters(password)
-      chars = 0
-      %i[number lowercase capital].each do |char_type|
-        chars += 1 if password.match?(send(:"#{char_type}_format"))
-      end
-      chars
-    end
-
-    def number_format
-      /\d/
-    end
-
-    def lowercase_format
-      /[a-z]/
-    end
-
-    def capital_format
-      /[A-Z]/
-    end
-  end
 end
