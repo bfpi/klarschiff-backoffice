58ebec0b072a178b3010deebba8dbc5b183a9c50
Niels Bennke <bennke@bfpi.de>
2022-03-08 22:40:09 +0100

    Überarbeitung und Erweiterung Tests

--- test/models/issue_email_test.rb
+++ test/models/issue_email_test.rb
@@ -5,20 +5,26 @@ require 'test_helper'
 class IssueEmailTest < ActiveSupport::TestCase
   include ActionMailer::TestHelper
 
-  %i[to_email text].each do |attr|
+  { to_email: 'test@abc.de', text: 'abc' }.each do |attr, value|
     test "validate #{attr} presence" do
       issue_email = IssueEmail.new
       assert_not issue_email.valid?
       assert_equal [{ error: :blank }], issue_email.errors.details[attr]
+      issue_email = IssueEmail.new(attr => value)
+      issue_email.valid?
+      assert_empty issue_email.errors.details[attr]
     end
   end
 
   %i[from_email to_email].each do |attr|
     test "validate #{attr} format" do
-      val = 'trwes.sdfsdf.de'
-      issue_email = IssueEmail.new attr => val
+      value = 'trwes.sdfsdf.de'
+      issue_email = IssueEmail.new(attr => value)
       assert_not issue_email.valid?
-      assert_equal [{ error: :email, value: val }], issue_email.errors.details[attr]
+      assert_equal [{ error: :email, value: value }], issue_email.errors.details[attr]
+      issue_email = IssueEmail.new(attr => 'test@abc.de')
+      issue_email.valid?
+      assert_empty issue_email.errors.details[attr]
     end
   end
 end
