c5870a9080616c001193021d3020f52bb7436f2c
Ricardo Oest <oest@bfpi.de>
2022-04-11 08:57:39 +0200

    Authorized-Scope für Außendienst. Anpassungen Fixtures und Tests

--- test/models/log_entry_test.rb
+++ test/models/log_entry_test.rb
@@ -7,7 +7,8 @@ class LogEntryTest < ActiveSupport::TestCase
     assert_equal LogEntry.ids, LogEntry.authorized(user(:admin)).ids
     user = user(:regional_admin2)
     assert LogEntry.authorized(user).any?
-    assert_not_equal LogEntry.ids, LogEntry.authorized(user).ids
+    assert_not_equal LogEntry.ids, (authorized_ids = LogEntry.authorized(user).ids)
+    assert_not log_entry(:mail_blacklist_changed).id.in?(authorized_ids)
     assert_empty LogEntry.authorized(user(:editor))
   end
 end
