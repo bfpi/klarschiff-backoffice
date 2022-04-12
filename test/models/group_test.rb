eeecf9461b5eb4843e1530c72a1780ffa2982572
Niels Bennke <bennke@bfpi.de>
2022-04-12 23:38:02 +0200

    Tests überarbeitet

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -5,15 +5,15 @@ require 'test_helper'
 class GroupTest < ActiveSupport::TestCase
   test 'validate no_associated_categories' do
     group = group(:one)
-    assert group.valid?
-    group.assign_attributes(active: false)
+    assert_valid group
+    group.active = false
     assert_not group.valid?
     assert_equal [{ error: :associated_categories }], group.errors.details[:base]
   end
 
   test 'deactivate group without associated categories' do
     group = group(:external)
-    assert group.valid?
+    assert_valid group
     assert group.update(active: false)
     assert_not group.reload.active
   end
