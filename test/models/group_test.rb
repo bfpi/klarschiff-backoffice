27e746b69eda385ca8455591f650ad69eccd225a
ro90 <oest@bfpi.de>
2022-04-12 23:41:38 +0200

    Validierung bei Gruppendeaktivierung mit Zuständigkeiten (#188)
    Co-authored-by: Niels Bennke <bennke@bfpi.de>

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -3,6 +3,21 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
+  test 'validate no_associated_categories' do
+    group = group(:one)
+    assert_valid group
+    group.active = false
+    assert_not group.valid?
+    assert_equal [{ error: :associated_categories }], group.errors.details[:base]
+  end
+
+  test 'deactivate group without associated categories' do
+    group = group(:external)
+    assert_valid group
+    assert group.update(active: false)
+    assert_not group.reload.active
+  end
+
   test 'authorized scope' do
     assert_equal Group.ids, Group.authorized(user(:admin)).ids
     assert_empty Group.authorized(user(:editor))
