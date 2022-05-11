c3ff99fe793b23f8722b758ee35951b6acfc0dc3
Ricardo Oest <oest@bfpi.de>
2022-05-11 09:27:13 +0200

    Mailer-Partial verschoben. default-group in E-Mail-Vorschau

--- test/mailers/previews/responsibility_mailer_preview.rb
+++ test/mailers/previews/responsibility_mailer_preview.rb
@@ -5,6 +5,10 @@ class ResponsibilityMailerPreview < ActionMailer::Preview
     ResponsibilityMailer.issue Issue.first, to: 'test@bfpi.de', auth_code: AuthCode.new(uuid: SecureRandom.uuid)
   end
 
+  def default_group
+    ResponsibilityMailer.default_group Issue.first, to: 'test@bfpi.de', auth_code: AuthCode.new(uuid: SecureRandom.uuid)
+  end
+
   def remind_group
     issues = Issue.where(responsibility_accepted: false).where.not(group_id: nil).limit(5)
     ResponsibilityMailer.remind_group Group.first, to: %w[test1@bfpi.de test2@bfpi.de], issues:
