312ce15ffd12fb1f5f427b6032beb670bed35286
Niels Bennke <bennke@bfpi.de>
2022-06-15 13:16:53 +0200

    Test zur Absicherung des ergänzten Status-Templates hinzugefügt

--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -126,4 +126,10 @@ class IssueTest < ActiveSupport::TestCase
     assert_not_equal Issue.ids, Issue.authorized(user).ids
     assert_empty Issue.authorized(user(:editor2))
   end
+
+  test 'ensure status not template value for responibility' do
+    value = Config.for(:status_note_template, env: nil)['Zuständigkeit']
+    assert_not_nil value
+    assert_not_empty value
+  end
 end
