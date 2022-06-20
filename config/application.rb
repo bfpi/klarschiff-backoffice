c896a74a02ab58f1da84d8aa761922835288cc98
Niels Bennke <bennke@bfpi.de>
2022-06-20 08:16:18 +0200

    `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails'
@@ -10,8 +8,8 @@ require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-# require "action_mailbox/engine"
-require 'action_text/engine'
+# require 'action_mailbox/engine'
+# require 'action_text/engine'
 require 'action_view/railtie'
 # require "action_cable/engine"
 require 'sprockets/railtie'
@@ -26,10 +24,16 @@ module KlarschiffBackoffice
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Europe/Berlin'
+
     config.active_record.pluralize_table_names = false
 
     config.action_mailer.show_previews = true
@@ -43,7 +47,5 @@ module KlarschiffBackoffice
 
     config.i18n.default_locale = :de
     config.i18n.available_locales = %i[de en]
-
-    config.time_zone = 'Europe/Berlin'
   end
 end
