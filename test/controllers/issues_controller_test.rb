4d48a4f8bd0c79f2cc59acae5ebcf88e75a8d704
Niels Bennke <bennke@bfpi.de>
2022-09-07 11:09:59 +0200

    Tests verbessert

--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -18,9 +18,9 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     expression = -> { LogEntry.where(issue_id: issue.id, attr: :responsibility_accepted, new_value: :Ja).count }
     assert_difference expression, 1 do
       patch "/issues/#{issue.id}.js", params: { issue: { responsibility_action: :accept } }
-      assert_response :success
-      assert_predicate issue.reload, :responsibility_accepted
     end
+    assert_response :success
+    assert_predicate issue.reload, :responsibility_accepted
   end
 
   test 'reject responsibility' do
@@ -30,9 +30,9 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     expression = -> { LogEntry.where(issue_id: issue.id, attr: :responsibility_accepted, new_value: :Nein).count }
     assert_difference expression, 1 do
       patch "/issues/#{issue.id}.js", params: { issue: { responsibility_action: :reject } }
-      assert_response :success
-      assert_not issue.reload.responsibility_accepted
     end
+    assert_response :success
+    assert_not issue.reload.responsibility_accepted
   end
 
   test 'close_as_not_solvable' do
@@ -44,10 +44,10 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     }
     assert_difference expression, 1 do
       patch "/issues/#{issue.id}.js", params: { issue: { responsibility_action: :close_as_not_solvable } }
-      assert_response :success
-      assert_not issue.reload.responsibility_accepted
-      assert_predicate issue, :status_not_solvable?
     end
+    assert_response :success
+    assert_not issue.reload.responsibility_accepted
+    assert_predicate issue, :status_not_solvable?
   end
 
   test 'manual responsibility change' do
@@ -59,10 +59,10 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     expression = -> { LogEntry.where(issue_id: issue.id, attr: 'group', new_value_id: group.id).count }
     assert_difference expression, 1 do
       patch "/issues/#{issue.id}.js", params: { issue: { group_id: group.id, responsibility_action: :manual } }
-      assert_response :success
-      assert_equal group, issue.reload.group
-      assert_not issue.responsibility_accepted
     end
+    assert_response :success
+    assert_equal group, issue.reload.group
+    assert_not issue.responsibility_accepted
   end
 
   test 'no changes on manual responsibility change to same group' do
@@ -70,10 +70,10 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     issue = issue(:received_not_accepted_two)
     assert_no_difference 'LogEntry.count' do
       patch "/issues/#{issue.id}.js", params: { issue: { group_id: issue.group_id, responsibility_action: :manual } }
-      assert_response :success
-      assert_equal issue.group_id, issue.reload.group_id
-      assert_not issue.responsibility_accepted
     end
+    assert_response :success
+    assert_equal issue.group_id, issue.reload.group_id
+    assert_not issue.responsibility_accepted
   end
 
   test 'recalculate responsibility' do
@@ -84,9 +84,9 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     expression = -> { LogEntry.where(issue_id: issue.id, attr: 'group', new_value_id: expected_target_group_id).count }
     assert_difference expression, 1 do
       patch "/issues/#{issue.id}.js", params: { issue: { responsibility_action: :recalculate } }
-      assert_response :success
-      assert_equal expected_target_group_id, issue.reload.group_id
     end
+    assert_response :success
+    assert_equal expected_target_group_id, issue.reload.group_id
   end
 
   test 'unknown responsibility_action' do
@@ -94,7 +94,7 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     issue = issue(:received)
     assert_no_difference -> { LogEntry.where(issue_id: issue.id).count } do
       patch "/issues/#{issue.id}.js", params: { issue: { responsibility_action: :test } }
-      assert_response :success
     end
+    assert_response :success
   end
 end
