9159ad6d14a5bd185e976531c4830147c99b12f0
Niels Bennke <bennke@bfpi.de>
2022-09-16 14:31:53 +0200

    Versuch den Aufruf erst beim tatsächlichen `include` auszuführen

--- app/controllers/concerns/arel_table.rb
+++ app/controllers/concerns/arel_table.rb
@@ -3,11 +3,13 @@
 module ArelTable
   extend ActiveSupport::Concern
 
-  private
+  included do
+    private
 
-  ActiveRecord::Base.connection.tables.each do |table|
-    define_method "#{table}_arel_table" do
-      table.camelize.constantize.arel_table
+    ActiveRecord::Base.connection.tables.each do |table|
+      define_method "#{table}_arel_table" do
+        table.camelize.constantize.arel_table
+      end
     end
   end
 end
