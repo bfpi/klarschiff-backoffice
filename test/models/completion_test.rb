e99148ba50c956e83e5f79fa6d700a627dfe27a5
Niels Bennke <bennke@bfpi.de>
2022-10-19 11:08:40 +0200

    Test des Mailversands konkretisiert

--- test/models/completion_test.rb
+++ test/models/completion_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 class CompletionTest < ActiveSupport::TestCase
   include ActionMailer::TestHelper
+
   test 'confirming completion closes issue' do
     completion = completion(:one)
     assert_not (issue = completion.issue).status_closed?
@@ -18,11 +19,12 @@ class CompletionTest < ActiveSupport::TestCase
     assert issue.valid?
     assert_predicate (author = completion.author), :present?
     completion.reject_with_status_reset = true
-    assert completion.update(status: 'rejected', notice: 'test')
+    assert_enqueued_email_with(CompletionMailer, :rejection, args: [{ to: author, completion: }]) do
+      assert completion.update(status: 'rejected', notice: 'test')
+    end
     assert_predicate completion.reload, :status_rejected?
     assert_nil completion.author
     assert_predicate issue.reload, :status_reviewed?
-    assert_enqueued_email_with(CompletionMailer, :rejection, args: [{ to: author, completion: }])
   end
 
   test 'reject completion after confirming completion for closed issue with closed completion' do
