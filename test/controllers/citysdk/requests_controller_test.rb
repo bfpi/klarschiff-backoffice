c3d224333259c83ad13adf76c2bb4feee3571726
ro3t <voss@bfpi.de>
2023-02-23 15:24:00 +0100

    Redmine #4096: Fehlermeldung in Zusammenhang mit CitySDK-Request-Filter (#241)
    Co-authored-by: Niels Bennke <bennke@bfpi.de>

--- test/controllers/citysdk/requests_controller_test.rb
+++ test/controllers/citysdk/requests_controller_test.rb
@@ -79,13 +79,27 @@ class RequestsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'index with extensions and area_code for idistricts with api-key frontend' do
+  test 'index with extensions and area_code for districts with api-key frontend' do
     with_parent_instance_settings(url: 'http://www.example.com') do
       get "/citysdk/requests.xml?extensions=true&area_code=#{district(:one).id}&api_key=#{api_key_frontend}"
       doc = Nokogiri::XML(response.parsed_body)
       requests = doc.xpath('/service_requests/request')
       assert requests.count.positive?
     end
+  end
+
+  test 'index with observation-key' do
+    get "/citysdk/requests.xml?observation_key=#{observation(:one).key}"
+    doc = Nokogiri::XML(response.parsed_body)
+    requests = doc.xpath('/service_requests/request')
+    assert requests.count.positive?
+  end
+
+  test 'index with unknown observation-key' do
+    get '/citysdk/requests.xml?observation_key=1234'
+    doc = Nokogiri::XML(response.parsed_body)
+    requests = doc.xpath('/service_requests/request')
+    assert requests.count.zero?
   end
 
   test 'index with extensions with api-key ppc' do
