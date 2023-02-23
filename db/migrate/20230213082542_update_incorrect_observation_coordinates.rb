9fcfa7735889bdc68aa6b92e6afdd9b6b36f2108
Niels Bennke <bennke@bfpi.de>
2023-02-23 09:01:47 +0100

    `find_each` statt `all.each` und SQL-Codestyle

--- db/migrate/20230213082542_update_incorrect_observation_coordinates.rb
+++ db/migrate/20230213082542_update_incorrect_observation_coordinates.rb
@@ -2,9 +2,9 @@
 
 class UpdateIncorrectObservationCoordinates < ActiveRecord::Migration[6.0]
   def up
-    Observation.all.each do |obs|
+    Observation.find_each do |obs|
       next if obs.area.geometry_n(0).coordinates.flatten.first.to_i < 100
-      sql = "UPDATE observation set area = ST_Transform(ST_SetSRID(area, 25833), 4326) where id = #{obs.id}"
+      sql = "UPDATE observation SET area = ST_Transform(ST_SetSRID(area, 25833), 4326) WHERE id = #{obs.id}"
       ActiveRecord::Base.connection.execute(sql)
     end
   end
