155e76006eff6d41a018e3d303bda405fc5cec92
Niels Bennke <bennke@bfpi.de>
2023-03-29 13:35:40 +0200

    Trac #50: Nur aktive Services via CitySDK liefern

--- test/controllers/citysdk/services_controller_test.rb
+++ test/controllers/citysdk/services_controller_test.rb
@@ -60,4 +60,26 @@ class ServicesControllerTest < ActionDispatch::IntegrationTest
     document_url = doc.xpath('/service_definition/service/document_url')
     assert_not_empty document_url
   end
+
+  [:api_key_ppc, :api_key_frontend, nil].each do |api_key_name|
+    test "index returns no inactive service for api-key #{api_key_name}" do
+      get '/citysdk/services.xml', params: api_key_name ? { api_key: send(api_key_name)} : {}
+      service_codes = Nokogiri::XML(response.parsed_body).xpath('/services/service/service_code')
+      assert_predicate service_codes.count, :positive?
+      assert_empty Service.joins(:main_category, :sub_category).where(
+        MainCategory.arel_table[:deleted].eq(true).or(SubCategory.arel_table[:deleted].eq(true))
+      ).where(id: service_codes.map(&:text))
+    end
+
+    %i[deleted deleted_main_category deleted_sub_category].each do |category_key|
+      test "show for inactive service #{category_key} for api-key #{api_key_name}" do
+        get "/citysdk/services/#{category(category_key).id}.xml", params: api_key_name ? { api_key: send(api_key_name)} : {}
+        service_codes = Nokogiri::XML(response.parsed_body).xpath('/service_definition/service/service_code')
+        assert_predicate service_codes.count, :positive?
+        assert_not_empty Service.joins(:main_category, :sub_category).where(
+          MainCategory.arel_table[:deleted].eq(true).or(SubCategory.arel_table[:deleted].eq(true))
+        ).where(id: service_codes.map(&:text))
+      end
+    end
+  end
 end
