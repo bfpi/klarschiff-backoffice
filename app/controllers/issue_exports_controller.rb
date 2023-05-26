c0121ea53610057d752ada1acbbafc7c9fba2d6e
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2023-05-26 09:23:46 +0200

    Autor auch nicht im PDF ausgeben

--- app/controllers/issue_exports_controller.rb
+++ app/controllers/issue_exports_controller.rb
@@ -6,9 +6,12 @@ class IssueExportsController < ApplicationController
   def create
     @issue = Issue.find(params[:issue_id])
     @map_image = Base64.decode64(params[:map_image])
-    kit = PDFKit.new(render_to_string(template: 'issue_exports/create', formats: :html, layout: false),
+    res = params[:restricted].try(:to_boolean) if params[:restricted].present?
+    kit = PDFKit.new(
+      render_to_string(template: 'issue_exports/create', formats: :html, layout: false, locals: { restricted: res }),
       root_url: "#{request.base_url}/", page_size: 'A4', margin_top: '0.5in', margin_right: '0.5in',
-      margin_bottom: '0.5in', margin_left: '0.5in')
+      margin_bottom: '0.5in', margin_left: '0.5in'
+    )
     headers['X-FileName'] = (file_name = "meldung-#{@issue.id}.pdf")
     send_data kit.to_pdf, filename: file_name, disposition: :attachment
   end
