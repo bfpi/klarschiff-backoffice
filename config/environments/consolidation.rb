0845f3196b7e6eea87e29b78500fa8f4a5f04fc4
rvoss <voss@bfpi.de>
2024-01-30 06:06:24 +0000

    RelativeUrlRoot wieder in die Environments

--- config/environments/consolidation.rb
+++ config/environments/consolidation.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.relative_url_root = '/backoffice'
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
