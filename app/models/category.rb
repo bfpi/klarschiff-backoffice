2fa866886f7ccc0670d0a5f5bb776d622e224440
rvoss <voss@bfpi.de>
2024-06-19 08:44:51 +0200

    Sortierreihenfolge explizit vorgegeben

--- app/models/category.rb
+++ app/models/category.rb
@@ -27,11 +27,20 @@ class Category < ApplicationRecord
   def group(lat:, lon:)
     group_ids = responsibilities.regional(lat:, lon:).pluck(:group_id)
     Group.regional(lat:, lon:).where(
-      Group.arel_table[:id].in(group_ids).or(Group.arel_table[:reference_default].eq(true))
-    ).order(:type, :reference_default).first
+      group_arel_table[:id].in(group_ids).or(group_arel_table[:reference_default].eq(true))
+    ).order(group_order).first
   end
 
   private
+
+  def group_type_arel_table
+    group_arel_table[:type]
+  end
+
+  def group_order
+    [group_type_arel_table.eq('AuthorityGroup'), group_type_arel_table.eq('CountyGroup'),
+     group_type_arel_table.eq('InstanceGroup'), :reference_default]
+  end
 
   def full_text_content
     [to_s, responsibilities.map { |x| x.group.to_s }].join(' ')
