ae103bccc4cbfd79d33abb61e8a53f4493dbec2f
Niels Bennke <bennke@bfpi.de>
2024-09-11 07:16:32 +0200

    Fix RuboCop Rails/EnumSyntax: Enum defined with keyword arguments in enum declaration. Use positional arguments instead.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ApplicationRecord
   include FullTextFilter
   include Logging
 
-  enum kind: { internal: 0, external: 1, field_service_team: 2 }, _prefix: true
+  enum :kind, { internal: 0, external: 1, field_service_team: 2 }, prefix: true
 
   belongs_to :main_user, class_name: 'User', optional: Settings::Group.main_user_optional
 
