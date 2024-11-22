7a96369701daef60ae8ad0a159362ebad76c7aba
Ricardo Oest <oest@bfpi.de>
2024-11-22 12:12:04 +0100

    Rails-Update -> v 7.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Only use :id for inspections in production.
+  config.active_record.attributes_for_inspect = %i[id]
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
