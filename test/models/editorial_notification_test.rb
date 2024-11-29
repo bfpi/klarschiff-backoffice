c2d7c9424f9916090a6eecc08a9b66fa1a3890d1
Niels Bennke <bennke@bfpi.de>
2024-11-29 10:30:01 +0100

    Nicht die identische Logik im Test nutzen

--- test/models/editorial_notification_test.rb
+++ test/models/editorial_notification_test.rb
@@ -6,10 +6,10 @@ class EditorialNotificationTest < ActiveSupport::TestCase
   test 'authorized scope' do
     assert_equal EditorialNotification.ids, EditorialNotification.authorized(user(:admin)).ids
     user = user(:regional_admin)
-    notifications = EditorialNotification.where(
-      user_id: User.authorized(user).map(&:id)
-    )
-    assert_equal notifications.ids, EditorialNotification.authorized(user).ids
+    notification_ids = EditorialNotification.where(user_id: User.joins(:groups)
+      .where(group: { id: Group.authorized(user) })).ids
+    assert_not_empty notification_ids
+    assert_equal notification_ids, EditorialNotification.authorized(user).ids
     assert_empty EditorialNotification.authorized(user(:editor)).ids
   end
 end
