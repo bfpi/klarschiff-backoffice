46f9c9322113d5ca86ab51993604e7136b6cda3b
Ralf Dostal <dostal@bfpi.de>
2025-06-04 07:31:11 +0200

    Alle deutschen Texte durch I18n ersetzt.

--- test/controllers/citysdk/requests/notes_controller_test.rb
+++ test/controllers/citysdk/requests/notes_controller_test.rb
@@ -6,20 +6,19 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
   test 'index without api-key' do
     get "/citysdk/requests/notes/#{issue(:one).id}.xml"
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '400', 'Es wurde kein API-Key übergeben.'
+    assert_error_messages doc, '400', '<%= I18n.t("test.controller.citysdk.error_400_message") %>'
   end
 
   test 'index with invalid api-key' do
     get "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_invalid}"
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '401', 'Der übergebene API-Key ist ungültig.'
+    assert_error_messages doc, '401', '<%= I18n.t("test.controller.citysdk.error_401_message") %>'
   end
 
   test 'index with api-key frontend' do
     get "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_frontend}"
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '403',
-      'Mit dem übergebenen API-Key stehen die benötigten Zugriffsrechte nicht zur Verfügung.'
+    assert_error_messages doc, '403', '<%= I18n.t("test.controller.citysdk.error_403_message") %>'
   end
 
   test 'index with api-key ppc' do
@@ -32,21 +31,20 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
     post "/citysdk/requests/notes/#{issue(:one).id}.xml",
       params: { author: 'test@example.com', comment: 'abcde' }
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '400', 'Es wurde kein API-Key übergeben.'
+    assert_error_messages doc, '400', '<%= I18n.t("test.controller.citysdk.error_400_message") %>'
   end
 
   test 'create with api-key frontend' do
     post "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_frontend}",
       params: { author: 'test@example.com', comment: 'abcde' }
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '403',
-      'Mit dem übergebenen API-Key stehen die benötigten Zugriffsrechte nicht zur Verfügung.'
+    assert_error_messages doc, '403', '<%= I18n.t("test.controller.citysdk.error_403_message") %>'
   end
 
   test 'create without attributes' do
     post "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_ppc}"
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '422', 'Gültigkeitsprüfung ist fehlgeschlagen'
+    assert_error_messages doc, '422', '<%= I18n.t("test.controller.citysdk.error_422_message.validation_failed") %>'
   end
 
   test 'create' do
@@ -67,14 +65,14 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
     post "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_ppc}",
       params: { comment: 'abcde' }
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '422', 'Gültigkeitsprüfung ist fehlgeschlagen'
+    assert_error_messages doc, '422', '<%= I18n.t("test.controller.citysdk.error_422_message.validation_failed") %>'
   end
 
   test 'create without comment' do
     post "/citysdk/requests/notes/#{issue(:one).id}.xml?api_key=#{api_key_ppc}",
       params: { author: 'test@example.com' }
     doc = Nokogiri::XML(response.parsed_body)
-    assert_error_messages doc, '422', 'Gültigkeitsprüfung ist fehlgeschlagen'
+    assert_error_messages doc, '422', '<%= I18n.t("test.controller.citysdk.error_422_message.validation_failed") %>'
   end
 
   test 'reject create without privacy_policy_accepted if required' do
