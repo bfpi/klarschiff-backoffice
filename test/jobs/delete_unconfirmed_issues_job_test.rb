e06418361e8791ad904b54a8c01dc455cdeee0c4
rvoss <voss@bfpi.de>
2025-12-15 11:03:40 +0100

    Test etwas ausgibiger

--- test/jobs/delete_unconfirmed_issues_job_test.rb
+++ test/jobs/delete_unconfirmed_issues_job_test.rb
@@ -4,6 +4,20 @@ require 'test_helper'
 
 class DeleteUnconfirmedIssuesJobTest < ActiveJob::TestCase
   test 'that job is performable' do
-    assert_nothing_raised { DeleteUnconfirmedIssuesJob.perform_now }
+    issues = unconfirmed_issues(JobSettings::Issue.deletion_deadline_days.days.ago)
+    assert_includes issues.map(&:id), issue(:unconfirmed_with_auth_code).id
+    assert_difference 'issues.count', -1 do
+      assert_nothing_raised { DeleteUnconfirmedIssuesJob.perform_now }
+    end
+  end
+
+  private
+
+  def unconfirmed_issues(time)
+    Issue.where(issue_arel_table[:status].eq('pending').and(issue_arel_table[:created_at].lt(time)))
+  end
+
+  def issue_arel_table
+    Issue.arel_table
   end
 end
