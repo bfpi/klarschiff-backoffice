e40d92554db9df71c15744698e4d56a40839accf
ro3t <voss@bfpi.de>
2025-12-15 12:00:31 +0100

    Fix: ForeignKeyViolation in DeleteUnconfirmedIssuesJob (#641)
    Co-authored-by: Niels Bennke <bennke@bfpi.de>

--- test/jobs/delete_unconfirmed_issues_job_test.rb
+++ test/jobs/delete_unconfirmed_issues_job_test.rb
@@ -4,6 +4,21 @@ require 'test_helper'
 
 class DeleteUnconfirmedIssuesJobTest < ActiveJob::TestCase
   test 'that job is performable' do
-    assert_nothing_raised { DeleteUnconfirmedIssuesJob.perform_now }
+    assert_includes issues.map(&:id), issue(:unconfirmed_with_auth_code).id
+    assert_difference 'issues.count', -1 do
+      assert_nothing_raised { DeleteUnconfirmedIssuesJob.perform_now }
+    end
+    assert_not_includes issues.map(&:id), issue(:unconfirmed_with_auth_code).id
+  end
+
+  private
+
+  def issues
+    time = JobSettings::Issue.deletion_deadline_days.days.ago
+    Issue.where(issue_arel_table[:status].eq('pending').and(issue_arel_table[:created_at].lt(time)))
+  end
+
+  def issue_arel_table
+    Issue.arel_table
   end
 end
