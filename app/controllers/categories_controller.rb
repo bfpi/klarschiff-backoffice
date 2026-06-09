4826068998ab52e9599d8a3917a44a7408b5b53f
Niels Bennke <bennke@bfpi.de>
2026-06-09 05:37:41 +0000

    Überarbeitung Codevorschlag

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,17 +27,12 @@ class CategoriesController < ApplicationController
 
   private
 
-  # Only allow redirection with sanitized parameters.
   def safe_redirect_params
-    page = Integer(params[:page] || 1) rescue 1
-    filter = (params[:filter] || {}).permit(:include_inactive, :text)
-    order_by = (params[:order_by] || {}).permit(:column, :dir)
-    {
-      action: :index,
-      page: page,
-      filter: filter.to_h.slice('include_inactive', 'text'),
-      order_by: order_by.to_h.slice('column', 'dir')
-    }
+    page = (p = params[:page].to_i).positive? ? p : 1
+    params.permit(
+      filter: [:include_inactive, :text],
+      order_by: [:column, :dir]
+    ).to_h.merge(action: :index, page: page)
   end
 
   def filter(collection)
