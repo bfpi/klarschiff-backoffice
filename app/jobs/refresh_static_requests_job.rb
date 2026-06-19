47dd57c7605e68d390dcb7912098a84911a4cb2a
rvoss <voss@bfpi.de>
2026-06-19 13:10:04 +0200

    Fix: RuboCop Issue

--- app/jobs/refresh_static_requests_job.rb
+++ app/jobs/refresh_static_requests_job.rb
@@ -6,8 +6,9 @@ class RefreshStaticRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    if (proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']).present? # Workaround for open-uri https-proxy problem
-      proxy = "http://#{proxy}" unless proxy.match(%r{^https?://})
+    # Workaround for open-uri https-proxy problem
+    if (proxy = ENV['HTTP_PROXY'] || ENV.fetch('http_proxy', nil)).present?
+      proxy = "http://#{proxy}" unless %r{^https?://}.match?(proxy)
       p_uri = URI.parse(proxy)
       Net::HTTP.Proxy p_uri.host, p_uri.port
     else
