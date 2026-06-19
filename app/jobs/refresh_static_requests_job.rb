27948ff5984c76197425861839828db397508923
rvoss <voss@bfpi.de>
2026-06-19 13:04:12 +0200

    Use Proxy in RefreshStaticRequestsJob

--- app/jobs/refresh_static_requests_job.rb
+++ app/jobs/refresh_static_requests_job.rb
@@ -6,6 +6,12 @@ class RefreshStaticRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    Net::HTTP.get URI.join("#{Settings::Instance.frontend_url}requests")
+    if (proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']).present? # Workaround for open-uri https-proxy problem
+      proxy = "http://#{proxy}" unless proxy.match(%r{^https?://})
+      p_uri = URI.parse(proxy)
+      Net::HTTP.Proxy p_uri.host, p_uri.port
+    else
+      Net::HTTP
+    end.get URI.join(Settings::Instance.frontend_url, 'requests')
   end
 end
