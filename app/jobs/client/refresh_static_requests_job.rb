40629110bfeae72c68d40e3744b658eff7ad7266
Niels Bennke <bennke@bfpi.de>
2026-06-26 14:38:51 +0200

    Umzug Job für Client in einen Client Namespace

---
+++ app/jobs/client/refresh_static_requests_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+class Client
+  class RefreshStaticRequestsJob < ApplicationJob
+    queue_as :default
+
+    def perform(*_args)
+      # Workaround for open-uri https-proxy problem
+      if (proxy = ENV['HTTP_PROXY'] || ENV.fetch('http_proxy', nil)).present?
+        proxy = "http://#{proxy}" unless %r{^https?://}.match?(proxy)
+        p_uri = URI.parse(proxy)
+        Net::HTTP.Proxy p_uri.host, p_uri.port
+      else
+        Net::HTTP
+      end.get URI.join(Settings::Instance.frontend_url, 'requests')
+    end
+  end
+end
