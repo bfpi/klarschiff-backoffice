9c789b15b23e23347888cb61d5e6402185477440
rvoss <voss@bfpi.de>
2026-08-20 08:30:34 +0200

    Weiterleiten von Meldungen im Verlauf ablegen

---
+++ test/mailers/issue_mailer_test.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class IssueMailerTest < ActionMailer::TestCase
+  test 'forward' do
+    with_log_issue_mailer_forward_settings(log_issue_mailer_forward: false) do
+      issue = issue(:in_process)
+      issue_email = IssueEmail.new(valid_params)
+      issue_email.issue_id = issue.id
+      issue_email.validate
+
+      assert_no_difference 'LogEntry.count' do
+        mail = IssueMailer.forward(issue_email:)
+
+        assert_equal 'Weitergeleitete Meldung aus dem Beteiligungsportal Klarschiff MV', mail.subject
+        assert_equal ['test@example.com'], mail.to
+        assert_equal ['from@example.com'], mail.from
+        assert_match 'die folgende Meldung wurde durch die/den Klarschiff-MV-Nutzer/-in  an Sie weitergeleitet',
+          mail.body.encoded
+      end
+    end
+  end
+
+  test 'forward with logging enabled' do
+    with_log_issue_mailer_forward_settings(log_issue_mailer_forward: true) do
+      issue = issue(:in_process)
+      issue_email = IssueEmail.new(valid_params)
+      issue_email.issue_id = issue.id
+      issue_email.validate
+
+      assert_difference 'LogEntry.count', 1 do
+        mail = IssueMailer.forward(issue_email:)
+
+        assert_equal 'Weitergeleitete Meldung aus dem Beteiligungsportal Klarschiff MV', mail.subject
+        assert_equal ['test@example.com'], mail.to
+        assert_equal ['from@example.com'], mail.from
+        assert_match 'die folgende Meldung wurde durch die/den Klarschiff-MV-Nutzer/-in  an Sie weitergeleitet',
+          mail.body.encoded
+      end
+    end
+  end
+
+  private
+
+  def valid_params
+    {
+      from: 'Absender Name', from_email: 'test@example.com', to_email: 'test@example.com', text: 'Lorem ipsum dolor',
+      send_map: 1, send_photos: 1, send_comments: 1, send_feedbacks: 1, send_abuse_reports: 1
+    }
+  end
+end
